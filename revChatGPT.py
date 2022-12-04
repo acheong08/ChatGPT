671b58d4fcbac16627959985cacf4c2e8ca0cdf4
Scott Leibrand <scottleibrand@gmail.com>
2022-12-03 22:46:38 -0800

    support speaking answers with 'say' and multiline input

--- revChatGPT.py
+++ revChatGPT.py
@@ -50,25 +50,68 @@ class Chatbot:
         message = response["message"]["content"]["parts"][0]
         return {'message':message, 'conversation_id':self.conversation_id, 'parent_id':self.parent_id}
 
+def get_input(prompt):
+  # prompt for input
+  lines = []
+  print(prompt,end="")
+  while True:
+      line = input()
+      if line == "":
+          break
+      lines.append(line)
+
+  # Join the lines, separated by newlines, and print the result
+  user_input = "\n".join(lines)
+  #print(user_input)
+  return user_input
+
+
 if __name__ == "__main__":
     print("""
-    ChatGPT - A simple chatbot using OpenAI's GPT-3 API
-    By: github.com/acheong08
+    ChatGPT - A command-line interface to OpenAI's ChatGPT (https://chat.openai.com/chat)
+    Repo: github.com/scottleibrand/ChatGPT
+    Forked from: github.com/acheong08/ChatGPT
     """)
     print("Type '!exit' to exit")
+    print("Press enter twice to submit your question.")
     with open("config.json", "r") as f:
             config = json.load(f)
     chatbot = Chatbot(config)
+    import subprocess
+    from subprocess import Popen
+    import sys
+
     while True:
-        prompt = input("You: ")
+        prompt = get_input("You: ")
         if prompt == "!exit":
             break
+        print("Please wait for ChatGPT to formulate its full response...")
         try:
             response = chatbot.get_chat_response(prompt)
         except Exception as e:
             print("Something went wrong!")
             print(e)
             continue
-        print("\n")
+        # Erase the previous line
+        sys.stdout.write("\033[F\033[K")
+
+        #print("\n")
         print("Chatbot:", response['message'])
         print("\n")
+
+        arguments=list(sys.argv)
+        del arguments[0]
+
+        if len(arguments)>1:
+            try:
+                process.terminate()
+            except NameError:
+                print("")
+            if len(arguments)>2:
+
+                # Use `python3 ./revChatGPT.py say -v Samantha -r 600`` to make a Mac speak the output
+                # using the Samantha voice at 600 words per minute (about 3x)
+                arguments.append('"' + response['message'] + '"')
+                process = Popen(arguments)
+            else:
+                process = Popen([arguments[1],"-r 400",response['message']])
