d5daf76c62dbaa67d32ac9e3ba6157d94fd96d30
Antonio Cheong <acheong@student.dalat.org>
2022-12-04 15:38:21 +0800

    Multiline input

--- src/revChatGPT/__main__.py
+++ src/revChatGPT/__main__.py
@@ -1,18 +1,37 @@
-from revChatGPT import Chatbot
+from revChatGPT.revChatGPT import Chatbot
+import json
+
+def get_input(prompt):
+  # prompt for input
+  lines = []
+  print(prompt,end="")
+  while True:
+      line = input()
+      if line == "":
+          break
+      lines.append(line)
+
+  # Join the lines, separated by newlines, and print the result
+  user_input = "\n".join(lines)
+  #print(user_input)
+  return user_input
+
 if __name__ == "__main__":
     print("""
     ChatGPT - A simple chatbot using OpenAI's GPT-3 API
     By: github.com/acheong08
     """)
     print("Type '!exit' to exit")
+    print("Press enter twice to submit your question.\n")
     with open("config.json", "r") as f:
             config = json.load(f)
     chatbot = Chatbot(config)
     while True:
-        prompt = input("You: ")
+        prompt = get_input("You: ")
         if prompt == "!exit":
             break
         try:
+            print("Please wait for ChatGPT to formulate its full response...")
             response = chatbot.get_chat_response(prompt)
         except Exception as e:
             print("Something went wrong!")
