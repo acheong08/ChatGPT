07bb02b3a05b09bef56f8689d4242d4ae148364a
Antonio Cheong <acheong@student.dalat.org>
2022-12-04 16:27:46 +0800

    Simple refactor header

--- src/revChatGPT/revChatGPT.py
+++ src/revChatGPT/revChatGPT.py
@@ -6,22 +6,22 @@ class Chatbot:
     config: json
     conversation_id: str
     parent_id: str
+    headers: dict
     def __init__(self, config, conversation_id=None):
         self.config = config
         self.conversation_id = conversation_id
         self.parent_id = self.generate_uuid()
+        self.headers = {
+            "Accept": "application/json",
+            "Authorization": "Bearer " + self.config['Authorization'],
+            "Content-Type": "application/json"
+        }
 
     def generate_uuid(self):
         uid = str(uuid.uuid4())
         return uid
         
     def get_chat_response(self, prompt):
-        Authorization = self.config["Authorization"]
-        headers = {
-            "Accept": "application/json",
-            "Authorization": "Bearer " + Authorization,
-            "Content-Type": "application/json"
-        }
         data = {
             "action":"next",
             "messages":[
@@ -33,7 +33,7 @@ class Chatbot:
             "parent_message_id":self.parent_id,
             "model":"text-davinci-002-render"
         }
-        response = requests.post("https://chat.openai.com/backend-api/conversation", headers=headers, data=json.dumps(data))
+        response = requests.post("https://chat.openai.com/backend-api/conversation", headers=self.headers, data=json.dumps(data))
         try:
             response = response.text.splitlines()[-4]
         except:
