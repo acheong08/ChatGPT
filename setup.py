72ba0ed16c5ab1962ccc3a872fd8015755051582
Antonio Cheong <acheong@student.dalat.org>
2022-12-05 21:29:38 +0800

    Update requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "revChatGPT",
-    version = "0.0.23",
+    version = "0.0.23.1",
     license = "GNU General Public License v2.0",
     author = "Antonio Cheong",
     author_email = "acheong@student.dalat.org",
@@ -12,7 +12,8 @@ setup(
     install_requires = [
         "requests",
         "tls-client",
-        "bs4"
+        "bs4",
+        "lxml"
     ],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
