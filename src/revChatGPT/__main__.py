0b497998d58afc82f0291fe5cfd120d15e79fcd3
Antonio Cheong <acheong@student.dalat.org>
2022-12-05 22:36:34 +0800

    Data streaming fails on MacOS. Set normal default

--- src/revChatGPT/__main__.py
+++ src/revChatGPT/__main__.py
@@ -1,5 +1,7 @@
 from revChatGPT.revChatGPT import Chatbot
 import json
+from sys import argv
+import textwrap
 
 def get_input(prompt):
   # prompt for input
@@ -53,28 +55,36 @@ if __name__ == "__main__":
                 continue
             elif prompt == "!exit":
                 break
-        import textwrap
+        
+        if '--stream' in argv:
+            messages = []
+            lines_printed=0
 
-        messages = []
-        lines_printed=0
-
-        try:
-            print("Chatbot: ")
-            formatted_parts = []
-            for message in chatbot.get_chat_response(prompt, output="stream"):
-                # Split the message by newlines
-                message_parts = message['message'].split('\n')
-
-                # Wrap each part separately
+            try:
+                print("Chatbot: ")
                 formatted_parts = []
-                for part in message_parts:
-                    formatted_parts.extend(textwrap.wrap(part, width=80))
-                    for formatted_line in formatted_parts:
-                        if (len(formatted_parts) > lines_printed+1):
-                            print(formatted_parts[lines_printed])
-                            lines_printed+=1
-            print(formatted_parts[lines_printed])
-        except Exception as e:
-            print("Something went wrong!")
-            print(e)
-            continue
+                for message in chatbot.get_chat_response(prompt, output="stream"):
+                    # Split the message by newlines
+                    message_parts = message['message'].split('\n')
+
+                    # Wrap each part separately
+                    formatted_parts = []
+                    for part in message_parts:
+                        formatted_parts.extend(textwrap.wrap(part, width=80))
+                        for formatted_line in formatted_parts:
+                            if (len(formatted_parts) > lines_printed+1):
+                                print(formatted_parts[lines_printed])
+                                lines_printed+=1
+                print(formatted_parts[lines_printed])
+            except Exception as e:
+                print("Something went wrong!")
+                print(e)
+                continue
+        else:
+            try:
+                print("Chatbot: ")
+                print(chatbot.get_chat_response(prompt)['message'])
+            except Exception as e:
+                print("Something went wrong!")
+                print(e)
+                continue
