40856c98dad469358e6ea699b81ea0c4d85eaab7
Antonio Cheong <acheong@student.dalat.org>
2022-12-06 20:52:20 +0800

    reverse bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="revChatGPT",
-    version="0.0.27.3",
+    version="0.0.27.4",
     license="GNU General Public License v2.0",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
