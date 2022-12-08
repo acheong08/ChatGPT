997a9c4f589ed52d4ae5c87d02e011f08d261cef
Antonio Cheong <acheong@student.dalat.org>
2022-12-08 21:41:49 +0800

    Bump OpenAIAuth version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="revChatGPT",
-    version="0.0.33.2",
+    version="0.0.33.3",
     license="GNU General Public License v2.0",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
@@ -15,7 +15,7 @@ setup(
     install_requires=[
         "requests",
         "httpx[http2]",
-        "OpenAIAuth",
+        "OpenAIAuth>=0.0.4",
     ],
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
