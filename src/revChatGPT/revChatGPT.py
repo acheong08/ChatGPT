28976fdbf8c4e8aa9a555f0e24cf6b3d39376265
adrgs <albastroiudragos@gmail.com>
2022-12-11 15:49:55 +0100

    Fix httpx LocalProtocolError

--- src/revChatGPT/revChatGPT.py
+++ src/revChatGPT/revChatGPT.py
@@ -275,7 +275,7 @@ class AsyncChatbot:
                 self.base_url + "api/auth/session",
                 headers={
                     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, "
-                    "like Gecko) Version/16.1 Safari/605.1.15 ",
+                    "like Gecko) Version/16.1 Safari/605.1.15",
                 },
             )
             # Check the response code
