d78605f0676f54de958d1475fb9ccaee6008f8bd
Antonio Cheong <acheong@student.dalat.org>
2022-12-11 10:48:03 +0800

    Change from next to variant

--- src/revChatGPT/revChatGPT.py
+++ src/revChatGPT/revChatGPT.py
@@ -220,7 +220,7 @@ class Chatbot:
         """
         self.refresh_session()  # Refreshing session token is fast
         data = {
-            "action": "next",
+            "action": "variant",
             "messages": [
                 {
                     "id": str(generate_uuid()),
