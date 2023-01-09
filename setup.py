349cba4282edbe055ced34d326b46930db08dbee
Antonio Cheong <acheong@student.dalat.org>
2023-01-09 19:38:51 +0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="revChatGPT",
-    version="0.1.0",
+    version="0.1.1",
     license="GNU General Public License v2.0",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
