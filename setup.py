e868c6f6e58356246ce3117cc5b5272ed229a620
Antonio Cheong <acheong@student.dalat.org>
2023-01-13 08:36:38 +0800

    fix versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="revChatGPT",
-    version="0.2",
+    version="0.2.0",
     license="GNU General Public License v2.0",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
