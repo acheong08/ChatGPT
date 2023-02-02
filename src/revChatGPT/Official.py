adb5d0755a88f158a29a4da7a358773380ad501b
Antonio Cheong <acheong@student.dalat.org>
2023-02-02 21:47:36 +0800

    remove unsupported feature

--- src/revChatGPT/Official.py
+++ src/revChatGPT/Official.py
@@ -53,7 +53,6 @@ class Chatbot:
             max_tokens=get_max_tokens(prompt),
             stop=["\n\n\n"],
             stream=stream,
-            disallowed_special=()
         )
 
     def _process_completion(self, user_request: str, completion: dict) -> dict:
@@ -158,7 +157,6 @@ class AsyncChatbot(Chatbot):
             max_tokens=get_max_tokens(prompt),
             stop=["\n\n\n"],
             stream=stream,
-            disallowed_special=()
         )
 
     async def ask(self, user_request: str, temperature: float = 0.5) -> dict:
