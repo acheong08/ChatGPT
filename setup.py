34819edb67ca3e294a7f4941393b6c60f00bb5de
Antonio Cheong <acheong@student.dalat.org>
2023-02-12 14:59:57 +0800

    update dependencies

--- setup.py
+++ setup.py
@@ -13,15 +13,11 @@ setup(
     py_modules=["Unofficial", "GPTserver", "Official", "Proxied"],
     url="https://github.com/acheong08/ChatGPT",
     install_requires=[
-        "undetected_chromedriver>=3.1.7",
-        "selenium>=4.7.2",
-        "tls_client>=0.1.7",
+        "asyncio",
+        "httpx",
+        "OpenAIAuth",
+        "tiktoken",
     ],
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    entry_points={
-        "console_scripts": [
-            "revChatGPT = revChatGPT.__main__:main",
-        ],
-    },
 )
