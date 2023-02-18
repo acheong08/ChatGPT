146b079fad47099ee402a1a51cb913972a5fb77a
Antonio Cheong <acheong@student.dalat.org>
2023-02-18 22:17:25 +0800

    fix unimported mods

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="revChatGPT",
-    version="2.3.1",
+    version="2.3.2",
     license="GNU General Public License v2.0",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
@@ -13,7 +13,7 @@ setup(
     py_modules=["Unofficial", "V2", "V1"],
     url="https://github.com/acheong08/ChatGPT",
     install_requires=[
-        "OpenAIAuth==0.3.0",
+        "OpenAIAuth==0.3.1",
         "requests",
     ],
     extras_require={
