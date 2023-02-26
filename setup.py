6327301e362e7509e201045700367b104fd404b4
kyrylogy <108933234+kyrylogy@users.noreply.github.com>
2023-02-26 08:43:52 +0100

    Add missing dependencies (#901)

--- setup.py
+++ setup.py
@@ -26,6 +26,10 @@ setup(
             "tls_client",
             "requests",
         ],
+        "official": [
+            "openai",
+            "tiktoken",
+        ],
     },
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
