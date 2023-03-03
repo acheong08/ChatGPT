7b644382d6c75d99466d9b6edd30673d91e03f44
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-03-03 19:19:37 +0100

    Code with some minor tweaks to make it more professional: (#1016)

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="revChatGPT",
     version="3.0.8",
-    license="GNU General Public License v2.0",
+    description="ChatGPT is a reverse engineering of OpenAI's ChatGPT API",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/acheong08/ChatGPT",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
-    description="ChatGPT is a reverse engineering of OpenAI's ChatGPT API",
+    license="GNU General Public License v2.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=["Unofficial", "V2", "V1", "V0", "V3"],
-    url="https://github.com/acheong08/ChatGPT",
     install_requires=[
         "OpenAIAuth==0.3.2",
         "requests[socks]",
@@ -32,6 +33,4 @@ setup(
             "tiktoken",
         ],
     },
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
 )
