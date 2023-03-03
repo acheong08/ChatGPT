d574271f7a1648b8fa8116f51debb19d2b655531
joshuabrink1 <joshuabrink1@gmail.com>
2023-03-03 11:35:46 +0200

    add command completion

--- src/revChatGPT/utils.py
+++ src/revChatGPT/utils.py
@@ -2,19 +2,24 @@ from prompt_toolkit import prompt
 from prompt_toolkit import PromptSession
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.history import InMemoryHistory
-
+from prompt_toolkit.completion import WordCompleter
+import re
 
 def create_session():
     session = PromptSession(history=InMemoryHistory())
     return session
 
+def create_completer(commands: list, pattern_str: str = "$"):
+    completer = WordCompleter(words=commands, pattern=re.compile(pattern_str))
+    return completer
 
-def get_input(session=None):
+def get_input(session: PromptSession = None, completer: WordCompleter = None):
     """
     Multiline input function.
     """
     if session:
         user_input = session.prompt(
+            completer=completer,
             multiline=True,
             auto_suggest=AutoSuggestFromHistory(),
         )
