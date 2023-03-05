0e49088b019c010bf129c387b544e3042d7def9a
joshuabrink1 <joshuabrink1@gmail.com>
2023-03-05 21:06:41 +0200

    improve engine check

--- src/revChatGPT/V3.py
+++ src/revChatGPT/V3.py
@@ -88,7 +88,7 @@ class Chatbot:
         """
         Get token count
         """
-        if self.engine != "gpt-3.5-turbo" and self.engine != "gpt-3.5-turbo-0301":
+        if self.engine not in ["gpt-3.5-turbo", "gpt-3.5-turbo-0301"]:
             raise NotImplementedError("Unsupported engine {self.engine}")
 
         encoding = tiktoken.encoding_for_model(self.engine)
