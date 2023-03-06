9dd7fb12c229bb7b7800ec060afa25447f367918
Daniel Parizher <105245560+Arborym@users.noreply.github.com>
2023-03-05 19:06:41 -0500

    Refactor on WebGPT (#1042)

--- src/revChatGPT/WebGPT.py
+++ src/revChatGPT/WebGPT.py
@@ -1,149 +1,252 @@
-import requests, time, json
+import json
+import sys
+import time
+from typing import NoReturn
+
 from duckduckgo_search import ddg
 
-#V1 and Unofficial are unnecessary, imo
+# V1 and Unofficial are unnecessary, imo
 import revChatGPT.V1 as V1
-#V2 is reportedly "killed by openai"
-#V3 has own implementation
 
-def _apiSearch(query: str, numResults: int, timePeriod: str='', region: str=''):
-    return ddg(query, region = region, time = timePeriod, max_results = numResults)
+# V2 is reportedly "killed by openai"
+# V3 has own implementation
 
-def _prepare_results(results):
-    i=1
-    res = ""
-    for result in results:
-        res+=f'[{i}] "{result["body"]}"\nURL: {result["href"]}\n\n'
-        i+=1
-    return res
 
-def _headers(results):
-    res = []
-    for result in results:
-        res.append((result["href"], result["title"]))
-    return res
+def _prepare_results(results: list) -> str:
+    return "".join(
+        f'[{i}] "{result["body"]}"\nURL: {result["href"]}\n\n'
+        for i, result in enumerate(results, start=1)
+    )
 
-def _compile_prompt(prompt, results, reply_in = 'undefined'):
-    return f'''Web search results:
+
+def _headers(results: list) -> list:
+    return [(result["href"], result["title"]) for result in results]
+
+
+def _compile_prompt(prompt: str, results: list, reply_in="undefined") -> str:
+    return f"""Web search results:
 
 {_prepare_results(results)}
 Current date: {time.strftime("%d.%m.%Y")}
 
 Instructions: Using the provided web search results, write a comprehensive reply to the given query, using search results as references. Make sure to cite results using [number](url) notation after the reference. If the provided search results refer to multiple subjects with the same name, write separate answers for each subject.
 Query: {prompt}
-Reply in {reply_in}'''
+Reply in {reply_in}"""
 
-def webify(text:str, query: str, numResults: int, timePeriod: str='', region: str='', reply_in: str='undefined'):
-    searchResults = _apiSearch(query, numResults, timePeriod, region)
+
+def webify(
+    text: str,
+    query: str,
+    numResults: int,
+    timePeriod: str = "",
+    region: str = "",
+    reply_in: str = "undefined",
+):
+    searchResults = ddg(query, numResults, timePeriod, region)
     return _compile_prompt(text, searchResults, reply_in), _headers(searchResults)
 
-def beatifyMarkdown(response: str, headers: list):
+
+def beatifyMarkdown(response: str, headers: list) -> str:
     """
     If the result will be displayed with markdown support, turns [i] style references into [i](url), which will look as clickable [i]
     If you are using beatifyPlaintext too, use this first
     """
     for i in range(len(headers)):
-        response = response.replace(f'[{i+1}]', f'[{i+1}]({headers[i][0]})')
+        response = response.replace(f"[{i+1}]", f"[{i+1}]({headers[i][0]})")
     return response
 
-def beatifyPlaintext(response: str, headers: list):
+
+def beatifyPlaintext(response: str, headers: list) -> str:
     """
     Puts the references at the end of the response. Can be used after beatifyMarkdown. Useful for displaying result in plaintext
     """
     refs = "\n"
     for i in range(len(headers)):
         refs += f"\n[{i+1}] - {headers[i][1]} ({headers[i][0]})"
-    return response+refs
+    return response + refs
+
 
 class WebChatbot(V1.Chatbot):
     def webAsk(
-        self, prompt, conversation_id=None, parent_id=None, timeout=360, #default Ask parameters
-        query=None, numResults=3, timePeriod: str='', region: str='', reply_in: str = 'undefined' #Webify parameters
-        ):
+        self,
+        prompt,
+        conversation_id=None,
+        parent_id=None,
+        timeout=360,  # default Ask parameters
+        query=None,
+        numResults=3,
+        timePeriod: str = "",
+        region: str = "",
+        reply_in: str = "undefined",  # Webify parameters
+    ):
         """
         Ask a question to the chatbot, but providing search results for query (if not specified, prompt is used)
         Keep in mind: this one, unlike all the other asks, returns tuple
         """
         query = query or prompt
-        webPrompt, headers = webify(text = prompt, query = query, numResults = numResults, timePeriod = timePeriod, region = region, reply_in = reply_in)
+        webPrompt, headers = webify(
+            text=prompt,
+            query=query,
+            numResults=numResults,
+            timePeriod=timePeriod,
+            region=region,
+            reply_in=reply_in,
+        )
         for data in self.ask(prompt, conversation_id, parent_id, timeout):
             yield data, headers
 
     def webAskPlaintext(
-        self, prompt, conversation_id=None, parent_id=None, timeout=360, #default Ask parameters
-        query=None, numResults=3, timePeriod: str='', region: str='', reply_in: str = 'undefined' #Webify parameters
-        ):
+        self,
+        prompt,
+        conversation_id=None,
+        parent_id=None,
+        timeout=360,  # default Ask parameters
+        query=None,
+        numResults=3,
+        timePeriod: str = "",
+        region: str = "",
+        reply_in: str = "undefined",  # Webify parameters
+    ):
         """
         Ask a question to the chatbot, but providing search results for query (if not specified, prompt is used)
         Returns message adapted for plaintext
         """
         query = query or prompt
-        webPrompt, headers = webify(text = prompt, query = query, numResults = numResults, timePeriod = timePeriod, region = region, reply_in = reply_in)
+        webPrompt, headers = webify(
+            text=prompt,
+            query=query,
+            numResults=numResults,
+            timePeriod=timePeriod,
+            region=region,
+            reply_in=reply_in,
+        )
         for data in self.ask(prompt, conversation_id, parent_id, timeout):
             data["message"] = beatifyPlaintext(data["message"], headers)
             yield data
 
     def webAskMarkdown(
-        self, prompt, conversation_id=None, parent_id=None, timeout=360, #default Ask parameters
-        query=None, numResults=3, timePeriod: str='', region: str='', reply_in: str = 'undefined' #Webify parameters
-        ):
+        self,
+        prompt,
+        conversation_id=None,
+        parent_id=None,
+        timeout=360,  # default Ask parameters
+        query=None,
+        numResults=3,
+        timePeriod: str = "",
+        region: str = "",
+        reply_in: str = "undefined",  # Webify parameters
+    ):
         """
         Ask a question to the chatbot, but providing search results for query (if not specified, prompt is used)
         Returns message adapted for markdown
         """
         query = query or prompt
-        webPrompt, headers = webify(text = prompt, query = query, numResults = numResults, timePeriod = timePeriod, region = region, reply_in = reply_in)
+        webPrompt, headers = webify(
+            text=prompt,
+            query=query,
+            numResults=numResults,
+            timePeriod=timePeriod,
+            region=region,
+            reply_in=reply_in,
+        )
         for data in self.ask(prompt, conversation_id, parent_id, timeout):
             data["message"] = beatifyMarkdown(data["message"], headers)
             yield data
 
+
 class WebAsyncChatbot(V1.AsyncChatbot):
     async def webAsk(
-        self, prompt, conversation_id=None, parent_id=None, timeout=360, #default Ask parameters
-        query=None, numResults=3, timePeriod: str='', region: str='', reply_in: str = 'undefined' #Webify parameters
-        ):
+        self,
+        prompt,
+        conversation_id=None,
+        parent_id=None,
+        timeout=360,  # default Ask parameters
+        query=None,
+        numResults=3,
+        timePeriod: str = "",
+        region: str = "",
+        reply_in: str = "undefined",  # Webify parameters
+    ):
         """
         Ask a question to the chatbot, but providing search results for query (if not specified, prompt is used)
         Keep in mind: this one, unlike all the other asks, returns tuple
         """
         query = query or prompt
-        webPrompt, headers = webify(text = prompt, query = query, numResults = numResults, timePeriod = timePeriod, region = region, reply_in = reply_in)
+        webPrompt, headers = webify(
+            text=prompt,
+            query=query,
+            numResults=numResults,
+            timePeriod=timePeriod,
+            region=region,
+            reply_in=reply_in,
+        )
         async for data in self.ask(prompt, conversation_id, parent_id, timeout):
             yield data, headers
 
     async def webAskPlaintext(
-        self, prompt, conversation_id=None, parent_id=None, timeout=360, #default Ask parameters
-        query=None, numResults=3, timePeriod: str='', region: str='', reply_in: str = 'undefined' #Webify parameters
-        ):
+        self,
+        prompt,
+        conversation_id=None,
+        parent_id=None,
+        timeout=360,  # default Ask parameters
+        query=None,
+        numResults=3,
+        timePeriod: str = "",
+        region: str = "",
+        reply_in: str = "undefined",  # Webify parameters
+    ):
         """
         Ask a question to the chatbot, but providing search results for query (if not specified, prompt is used)
         Returns message adapted for plaintext
         """
         query = query or prompt
-        webPrompt, headers = webify(text = prompt, query = query, numResults = numResults, timePeriod = timePeriod, region = region, reply_in = reply_in)
+        webPrompt, headers = webify(
+            text=prompt,
+            query=query,
+            numResults=numResults,
+            timePeriod=timePeriod,
+            region=region,
+            reply_in=reply_in,
+        )
         async for data in self.ask(prompt, conversation_id, parent_id, timeout):
             data["message"] = beatifyPlaintext(data["message"], headers)
             yield data
 
     async def webAskMarkdown(
-        self, prompt, conversation_id=None, parent_id=None, timeout=360, #default Ask parameters
-        query=None, numResults=3, timePeriod: str='', region: str='', reply_in: str = 'undefined' #Webify parameters
-        ):
+        self,
+        prompt,
+        conversation_id=None,
+        parent_id=None,
+        timeout=360,  # default Ask parameters
+        query=None,
+        numResults=3,
+        timePeriod: str = "",
+        region: str = "",
+        reply_in: str = "undefined",  # Webify parameters
+    ):
         """
         Ask a question to the chatbot, but providing search results for query (if not specified, prompt is used)
         Returns message adapted for markdown
         """
         query = query or prompt
-        webPrompt, headers = webify(text = prompt, query = query, numResults = numResults, timePeriod = timePeriod, region = region, reply_in = reply_in)
+        webPrompt, headers = webify(
+            text=prompt,
+            query=query,
+            numResults=numResults,
+            timePeriod=timePeriod,
+            region=region,
+            reply_in=reply_in,
+        )
         async for data in self.ask(prompt, conversation_id, parent_id, timeout):
             data["message"] = beatifyMarkdown(data["message"], headers)
             yield data
-    
+
 
 get_input = V1.logger(is_timed=False)(V1.get_input)
 
+
 @V1.logger(is_timed=False)
-def main(config: dict):
+def main(config: dict) -> NoReturn:
     """
     Main function for the chatGPT program.
     """
@@ -195,7 +298,7 @@ def main(config: dict):
                 )
                 print("Please include conversation UUID in command")
         elif command == "!exit":
-            exit(0)
+            sys.exit(0)
         else:
             return False
         return True
@@ -210,9 +313,8 @@ def main(config: dict):
             print(V1.bcolors.OKBLUE + V1.bcolors.BOLD + "You:" + V1.bcolors.ENDC)
 
             prompt = get_input(session=session, completer=completer)
-            if prompt.startswith("!"):
-                if handle_commands(prompt):
-                    continue
+            if prompt.startswith("!") and handle_commands(prompt):
+                continue
 
             print()
             print(V1.bcolors.OKGREEN + V1.bcolors.BOLD + "Chatbot: ")
@@ -225,7 +327,8 @@ def main(config: dict):
             print()
     except (KeyboardInterrupt, EOFError):
         print("Exiting...")
-        exit(0)
+        sys.exit(0)
+
 
 if __name__ == "__main__":
     print(
@@ -235,5 +338,7 @@ if __name__ == "__main__":
         """,
     )
     print("Type '!help' to show a full list of commands")
-    print(f'{V1.bcolors.BOLD} {V1.bcolors.WARNING} Press Esc followed by Enter or Alt+Enter to send a message. {V1.bcolors.ENDC}')
+    print(
+        f"{V1.bcolors.BOLD} {V1.bcolors.WARNING} Press Esc followed by Enter or Alt+Enter to send a message. {V1.bcolors.ENDC}"
+    )
     main(configure())
