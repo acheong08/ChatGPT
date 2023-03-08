fdef83fdf5d251af5cc92ff0d9d60b7cf34e9cc9
Liem Luttrell <98436755+EpicLiem@users.noreply.github.com>
2023-03-07 19:27:28 -0500

    Added the ability for users to use config files with V3 (#1077)
    Co-authored-by: Daniel Parizher <105245560+Arborym@users.noreply.github.com>
    Co-authored-by: Antonio Cheong <acheong@student.dalat.org>

--- src/revChatGPT/V3.py
+++ src/revChatGPT/V3.py
@@ -40,6 +40,15 @@ class Chatbot:
         self.session = requests.Session()
         self.api_key = api_key
         self.proxy = proxy
+
+        self.system_prompt = system_prompt
+        self.max_tokens = max_tokens
+        self.temperature = temperature
+        self.top_p = top_p
+        self.presence_penalty = presence_penalty
+        self.frequency_penalty = frequency_penalty
+        self.reply_count = reply_count
+
         if self.proxy:
             proxies = {
                 "http": self.proxy,
@@ -54,15 +63,8 @@ class Chatbot:
                 },
             ],
         }
-        self.system_prompt = system_prompt
         if max_tokens > 4000:
             raise Exception("Max tokens cannot be greater than 4000")
-        self.max_tokens = max_tokens
-        self.temperature = temperature
-        self.top_p = top_p
-        self.presence_penalty = presence_penalty
-        self.frequency_penalty = frequency_penalty
-        self.reply_count = reply_count
 
         if self.get_token_count("default") > self.max_tokens:
             raise Exception("System prompt is too long")
@@ -245,6 +247,41 @@ class Chatbot:
             return False
         return True
 
+    def load_config(self, file: str, no_api_key: bool = False) -> bool:
+        """
+        Load the configuration from a JSON file
+        """
+        try:
+            with open(file, encoding="utf-8") as f:
+                config = json.load(f)
+                if config is not None:
+                    self.api_key = config.get("api_key") or self.api_key
+                    if self.api_key is None:
+                        # Make sure the API key is set
+                        raise Exception("Error: API key is not set")
+                    self.engine = config.get("engine") or self.engine
+                    self.temperature = config.get("temperature") or self.temperature
+                    self.top_p = config.get("top_p") or self.top_p
+                    self.presence_penalty = config.get("presence_penalty") or self.presence_penalty
+                    self.frequency_penalty = config.get("frequency_penalty") or self.frequency_penalty
+                    self.reply_count = config.get("reply_count") or self.reply_count
+                    self.max_tokens = config.get("max_tokens") or self.max_tokens
+
+                    if config.get("system_prompt") is not None:
+                        self.system_prompt = config.get("system_prompt") or self.system_prompt
+                        self.reset(system_prompt=self.system_prompt)
+
+                    if config.get("proxy") is not None:
+                        self.proxy = config.get("proxy") or self.proxy
+                        proxies = {
+                            "http": self.proxy,
+                            "https": self.proxy,
+                        }
+                        self.session.proxies = proxies
+        except (FileNotFoundError, KeyError, json.decoder.JSONDecodeError):
+            return False
+        return True
+
 
 class ChatbotCLI(Chatbot):
     def print_config(self, convo_id: str = "default") -> None:
@@ -281,6 +318,7 @@ Commands:
 
 Config Commands:
   !config          Display the current config
+  !load_config fileLoad the config from a JSON file
   !temperature n   Set the temperature to n
   !top_p n         Set the top_p to n
   !reply_count n   Set the reply_count to n
@@ -322,6 +360,11 @@ Config Commands:
                 )
             else:
                 print(f"Error: {value[0]} could not be loaded")
+        elif command == "!load_config":
+            if is_loaded := self.load_config(*value):
+                print(f"Loaded config from {value[0]}")
+            else:
+                print(f"Error: {value[0]} could not be loaded")
         elif command == "!temperature":
             self.temperature = float(value[0])
             print(f"\nTemperature set to {value[0]}")
@@ -358,7 +401,6 @@ def main() -> NoReturn:
     parser.add_argument(
         "--api_key",
         type=str,
-        required=True,
         help="OpenAI API key",
     )
     parser.add_argument(
@@ -402,6 +444,11 @@ def main() -> NoReturn:
         help="Allow ChatGPT to search the internet",
     )
     parser.add_argument(
+        "--config",
+        type=str,
+        help="Path to config.v3.json",
+    )
+    parser.add_argument(
         "--submit_key",
         type=str,
         default=None,
@@ -409,17 +456,30 @@ def main() -> NoReturn:
         Custom submit key for chatbot. For more information on keys, see README
         """,
     )
-
     args = parser.parse_args()
-    # Initialize chatbot
-    chatbot = ChatbotCLI(
-        api_key=args.api_key,
-        system_prompt=args.base_prompt,
-        proxy=args.proxy,
-        temperature=args.temperature,
-        top_p=args.top_p,
-        reply_count=args.reply_count,
-    )
+    # Load config
+    if args.config is not None:
+        # Initialize chatbot
+        chatbot = ChatbotCLI("placeholder")
+        no_api_key = args.api_key is None
+        chatbot.load_config(args.config, no_api_key=no_api_key)
+    else:
+        if args.api_key is None:
+            print(
+                "Add a config.v3.json and add the path using --config or add an api_key using --api_key"
+            )
+            # raising at top level is messy and can confuse some people
+            return
+
+        chatbot = ChatbotCLI(
+            api_key=args.api_key,
+            system_prompt=args.base_prompt,
+            proxy=args.proxy,
+            temperature=args.temperature,
+            top_p=args.top_p,
+            reply_count=args.reply_count,
+        )
+
     # Check if internet is enabled
     if args.enable_internet:
         from importlib.resources import path
@@ -441,8 +501,10 @@ def main() -> NoReturn:
             "!reply_count",
             "!save",
             "!load",
+            "!load_config",
         ],
     )
+    key_bindings = create_keybindings()
     if args.submit_key:
         key_bindings = create_keybindings(args.submit_key)
     # Start chat
