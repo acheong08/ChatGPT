f03fc6474167cee621b56bc28c0e6468541c55df
Antonio Cheong <acheong@student.dalat.org>
2023-03-10 10:21:44 +0800

    Revert "Improved version (#1104)"
    This reverts commit 02f3ec158fcf2d29479532b509097967fa975d21.

--- src/revChatGPT/utils.py
+++ src/revChatGPT/utils.py
@@ -1,10 +1,13 @@
 import re
 
-from prompt_toolkit import PromptSession, print_formatted_text
+from prompt_toolkit import prompt
+from prompt_toolkit import PromptSession
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.history import InMemoryHistory
 from prompt_toolkit.key_binding import KeyBindings
+
+bindings = KeyBindings()
 
 
 def create_keybindings(key: str = "c-@") -> KeyBindings:
@@ -12,8 +15,6 @@ def create_keybindings(key: str = "c-@") -> KeyBindings:
     Create keybindings for prompt_toolkit. Default key is ctrl+space.
     For possible keybindings, see: https://python-prompt-toolkit.readthedocs.io/en/stable/pages/advanced_topics/key_bindings.html#list-of-special-keys
     """
-
-    bindings = KeyBindings()
 
     @bindings.add(key)
     def _(event):
@@ -34,53 +35,35 @@ def get_input(
     session: PromptSession = None,
     completer: WordCompleter = None,
     key_bindings=None,
-    prompt_text: str = "",
 ) -> str:
     """
     Multiline input function.
     """
-
-    prompt_kwargs = {
-        "completer": completer,
-        "multiline": True,
-        "auto_suggest": AutoSuggestFromHistory(),
-        "key_bindings": key_bindings,
-        "prompt_continuation": "",
-        "prompt_kwargs": {"style": "class:prompt"},
-    }
-
     return (
         session.prompt(
-            prompt_text=prompt_text,
-            **prompt_kwargs,
+            completer=completer,
+            multiline=True,
+            auto_suggest=AutoSuggestFromHistory(),
+            key_bindings=key_bindings,
         )
         if session
-        else print_formatted_text(prompt_text) or input()
+        else prompt(multiline=True)
     )
 
 
 async def get_input_async(
     session: PromptSession = None,
     completer: WordCompleter = None,
-    prompt_text: str = "",
 ) -> str:
     """
     Multiline input function.
     """
-
-    prompt_kwargs = {
-        "completer": completer,
-        "multiline": True,
-        "auto_suggest": AutoSuggestFromHistory(),
-        "prompt_continuation": "",
-        "prompt_kwargs": {"style": "class:prompt"},
-    }
-
     return (
         await session.prompt_async(
-            prompt_text=prompt_text,
-            **prompt_kwargs,
+            completer=completer,
+            multiline=True,
+            auto_suggest=AutoSuggestFromHistory(),
         )
         if session
-        else print_formatted_text(prompt_text) or input()
+        else prompt(multiline=True)
     )
