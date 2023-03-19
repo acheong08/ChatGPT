d74879bf869b855996b7b92a11a3ffb208203f53
Antonio Cheong <acheong@student.dalat.org>
2023-03-19 11:03:23 +0800

    typings (version bump)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="revChatGPT",
-    version="4.0.4",
+    version="4.0.5",
     description="ChatGPT is a reverse engineering of OpenAI's ChatGPT API",
     long_description=open("README.md", "rt", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
