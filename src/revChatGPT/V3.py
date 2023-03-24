3b4b0153ff0888a0aed55e50a12fa26e69563a67
Antonio Cheong <acheong@student.dalat.org>
2023-03-24 09:31:23 +0800

    count more tokens just in case

--- src/revChatGPT/V3.py
+++ src/revChatGPT/V3.py
@@ -120,12 +120,12 @@ class Chatbot:
         num_tokens = 0
         for message in self.conversation[convo_id]:
             # every message follows <im_start>{role/name}\n{content}<im_end>\n
-            num_tokens += 4
+            num_tokens += 5
             for key, value in message.items():
                 num_tokens += len(encoding.encode(value))
                 if key == "name":  # if there's a name, the role is omitted
-                    num_tokens += 1  # role is always required and always 1 token
-        num_tokens += 2  # every reply is primed with <im_start>assistant
+                    num_tokens += 5  # role is always required and always 1 token
+        num_tokens += 5  # every reply is primed with <im_start>assistant
         return num_tokens
 
     def get_max_tokens(self, convo_id: str) -> int:
@@ -172,7 +172,7 @@ class Chatbot:
                 "user": role,
                 "max_tokens": self.get_max_tokens(convo_id=convo_id),
             },
-            timeout=kwargs.get('timeout', self.timeout),
+            timeout=kwargs.get("timeout", self.timeout),
             stream=True,
         )
         if response.status_code != 200:
