5011f84b0bd35b5e14369175e875928ff4fb3102
Antonio Cheong <acheong@student.dalat.org>
2023-04-01 10:30:10 +0800

    restore version checks

---
+++ src/revChatGPT/__init__.py
@@ -0,0 +1,16 @@
+# Available Python Version Verify
+from . import typings as t
+from platform import python_version_tuple
+from platform import python_version
+versions = [int(v) for v in python_version_tuple()]
+if versions[0] < 3:
+    error = t.NotAllowRunning("Not available Python version")
+    raise error
+elif versions[1] < 9:
+    error = t.NotAllowRunning(f"Not available Python version: {python_version()}")
+    raise error
+else:
+    del t, python_version, python_version_tuple, versions
+
+# Import Codes
+from . import V0, V1, V3
