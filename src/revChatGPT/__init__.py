d8482ee250e390d192b52ba04fa25fadbe62e0b7
CoolPlayLin <CoolPlayLin@qq.com>
2023-04-01 10:56:26 +0800

    add warn

--- src/revChatGPT/__init__.py
+++ src/revChatGPT/__init__.py
@@ -1,7 +1,18 @@
+"""
+The __init__ file does not a main file
+
+You can import the following module to use:
+revChatGPT.V0
+revChatGPT.V1
+revChatGPT.V3
+"""
+__all__ = ()
+
 # Available Python Version Verify
 from . import typings as t
 from platform import python_version_tuple
 from platform import python_version
+from warnings import warn
 versions = [int(v) for v in python_version_tuple()]
 if versions[0] < 3:
     error = t.NotAllowRunning("Not available Python version")
@@ -10,4 +21,8 @@ elif versions[1] < 9:
     error = t.NotAllowRunning(f"Not available Python version: {python_version()}")
     raise error
 else:
-    del t, python_version, python_version_tuple, versions
+    if versions[1] < 11 and versions[0] == 3:
+        warning = UserWarning("The current Python is not a recommended version, 3.11+ is recommended")
+        warn(warning)
+        del warning
+    del t, python_version, python_version_tuple, versions, warn
