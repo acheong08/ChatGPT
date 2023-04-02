c42ef3c21d74c10bf7754d1d1846405e715a824d
Antonio Cheong <acheong@student.dalat.org>
2023-04-03 00:02:29 +0800

    auth bugfix

--- src/revChatGPT/V1.py
+++ src/revChatGPT/V1.py
@@ -325,7 +325,9 @@ class Chatbot:
         )
         if self.config.get("session_token"):
             log.debug("Using session token")
-            auth.session_token = self.config["session_token"]
+            auth.session.cookies.set(
+                "__Secure-next-auth.session-token", self.config["session_token"]
+            )
             auth.get_access_token()
             if auth.access_token is None:
                 del self.config["session_token"]
@@ -334,7 +336,6 @@ class Chatbot:
         else:
             log.debug("Using authenticator to get access token")
             auth.begin()
-            self.config["session_token"] = auth.session_token
             auth.get_access_token()
 
         self.set_access_token(auth.access_token)
