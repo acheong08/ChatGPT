9a5329e99a9fb9e12d0224efb208b0faada939f3
CoolPlayLin <coolplaylin@qq.com>
2023-04-28 20:21:13 +0800

    fix (#1371)
    Co-authored-by: Sheng Fan <41846047+fred913@users.noreply.github.com>
    Co-authored-by: Antonio Cheong <acheong@student.dalat.org>

--- src/revChatGPT/__init__.py
+++ src/revChatGPT/__init__.py
@@ -8,24 +8,29 @@ revChatGPT.V3
 __version__ = "5.0.0"
 __all__ = ()
 
-# Available Python Version Verify
-from . import typings as t
 
-if int(__import__("platform").python_version_tuple()[0]) < 3:
-    error = t.NotAllowRunning("Not available Python version")
-    raise error
-elif int(__import__("platform").python_version_tuple()[1]) < 9:
-    error = t.NotAllowRunning(
-        f"Not available Python version: {__import__('platform').python_version()}",
-    )
-    raise error
-else:
-    if (
-        int(__import__("platform").python_version_tuple()[1]) < 10
-        and int(__import__("platform").python_version_tuple()[0]) == 3
-    ):
-        __import__("warnings").warn(
-            UserWarning(
-                "The current Python is not a recommended version, 3.10+ is recommended",
-            ),
+def verify() -> None:
+    # Available Python Version Verify
+    from . import typings as t
+
+    if int(__import__("platform").python_version_tuple()[0]) < 3:
+        error = t.NotAllowRunning("Not available Python version")
+        raise error
+    elif int(__import__("platform").python_version_tuple()[1]) < 9:
+        error = t.NotAllowRunning(
+            f"Not available Python version: {__import__('platform').python_version()}",
         )
+        raise error
+    else:
+        if (
+            int(__import__("platform").python_version_tuple()[1]) < 10
+            and int(__import__("platform").python_version_tuple()[0]) == 3
+        ):
+            __import__("warnings").warn(
+                UserWarning(
+                    "The current Python is not a recommended version, 3.10+ is recommended",
+                ),
+            )
+
+
+verify()
