967277e8004c9744f635466654950a3c534001d6
Antonio <acheong@student.dalat.org>
2023-06-08 01:11:12 +0800

    6.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if not PATH.exists():
 
 setup(
     name="revChatGPT",
-    version="6.1.1",
+    version="6.1.2",
     description="ChatGPT is a reverse engineering of OpenAI's ChatGPT API",
     long_description=open(PATH, encoding="utf-8").read(),
     long_description_content_type="text/markdown",
