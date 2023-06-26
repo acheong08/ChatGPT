a2d552042ac8d55103dd3b21e8d6fa7fc28aef30
editor99 <77914543+Gteditor99@users.noreply.github.com>
2023-06-27 00:50:13 +0900

    Add supported list of engines to `NotImplementedError` (#1438)
    * Fix "Catching too general exception `Exception`"
    
    * 6.4.4
    
    * Print supported engines
    
    * Test
    
    * Add supported list of engines to `NotImplementedError`
    
    ---------
    
    Co-authored-by: Antonio Cheong <acheong@student.dalat.org>

--- src/revChatGPT/V3.py
+++ src/revChatGPT/V3.py
@@ -154,8 +154,7 @@ class Chatbot:
         Get token count
         """
         if self.engine not in ENGINES:
-            raise NotImplementedError(f"Unsupported engine {self.engine}")
-
+            raise NotImplementedError(f"Engine {self.engine} is not supported. Select from {ENGINES}")
         tiktoken.model.MODEL_TO_ENCODING["gpt-4"] = "cl100k_base"
 
         encoding = tiktoken.encoding_for_model(self.engine)
